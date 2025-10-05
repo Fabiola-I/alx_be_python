d7b86592bc1e6f1b1504dbf1c3c74216732922bd
Fabi-starr <fabiolaingabire02@gmail.com>
2025-10-05 21:14:24 +0200

    Fixed BankAccount for ALX Task 0 checks

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,16 +5,14 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited ${amount}. New balance: ${self.account_balance}")
+        return self.account_balance  # return new balance for testing
 
     def withdraw(self, amount):
         if amount > self.account_balance:
-            print("Insufficient funds!")
             return False
         else:
             self.account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.account_balance}")
-            return True
+            return True  # return True to indicate success
 
     def display_balance(self):
-        print(f"Current account balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
